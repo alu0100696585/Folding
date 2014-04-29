b3f5871365b9ddc11829444f412a816d373ebd80
Cristian Luis <alu0100693386@ull.edu.es>
2014-04-29 16:36:12 +0100

    Modificacion el oauth para la introduccion de nuevos usuarios en la base de datos

--- auth.rb
+++ auth.rb
@@ -14,9 +14,20 @@ get '/auth/:name/callback' do
   session[:auth] = @auth = request.env['omniauth.auth']
   session[:name] = @auth['info'].name
   session[:image] = @auth['info'].image
+  #Existe la posibilidad de que el nombre de usuario tenga espacios, lo que puede provocar errores en las url.
+  #Para evitarlo usaremos el email.
+  session[:email] = @auth['info'].email
+  
+  PP.pp @auth.methods.sort
   
   flash[:notice] = 
         %Q{<div class="success">Authenticated as #{@auth['info'].name}.</div>}
+  
+  if !Autor.first(:user => session[:email])
+    u = Autor.create(:user => session[:email])
+    u.save
+  end
+  
   redirect '/'
 end
 
