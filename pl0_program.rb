95ae432eefbef6099edfacb5c29954d2bd95c9e3
Cristian Luis <alu0100693386@ull.edu.es>
2014-04-29 16:26:25 +0100

    Modificacion del modelo de datos en la base de datos

--- pl0_program.rb
+++ pl0_program.rb
@@ -1,13 +1,24 @@
 require 'data_mapper'
 # full path!
 DataMapper.setup(:default, 
-                 ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/database.db" )
+                 ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/database.db")
 
-class PL0Program
+class PL0program
   include DataMapper::Resource
   
-  property :name, String, :key => true
+  property :id, Serial
+  property :name, String #Este atributo deja de ser clave.
   property :source, String, :length => 1..1024
+  
+  belongs_to :autor
+end
+
+class Autor
+  include DataMapper::Resource
+  
+  property :user, String, :key => true
+  has n, :Pl0programs
+  
 end
 
 DataMapper.finalize
