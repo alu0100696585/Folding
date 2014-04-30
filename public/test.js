ce3baf019b5790a1064dfd5acc408dea5eb1ddb1
Cristian Luis <alu0100693386@ull.edu.es>
2014-04-30 15:12:06 +0100

    Preparando pruebas 2

--- public/test.js
+++ public/test.js
@@ -3,18 +3,28 @@ var assert = chai.assert; //la variable assert contiene los asertos que se puede
 suite( 'Analizador Sintacsis + Ambito', function(){  //Suite equivale al describle en RAKE
   
   test('Ambito procedure.', function(){  
-    var result = calculator.parse("");
+    var result = calculator.parse("var a, b, c; procedure doble(k); k = k * 2; procedure triple(k); k = k * 3; call double(a); call double(b); .");
     assert.deepEqual(JSON.stringify(result,undefined,2), '[\n  {\n    "st": {\n      "type": "ID",\n      "left": {\n        "type": "-",\n        "left": {\n          "type": "-",\n          "left": 3,\n          "right": 4\n        },\n        "right": 1\n      }\n    }\n  },\n  {\n    "A": {\n      "type": "ID",\n      "left": {\n        "type": "-",\n        "left": {\n          "type": "-",\n          "left": 3,\n          "right": 4\n        },\n        "right": 1\n      }\n    }\n  }\n]'); 
   });
   
   test('Variable no definida.', function(){  
-    var result = calculator.parse("var a, b; procedure jruvi(); h = a + b; b = h;.");
-    assert.deepEqual(JSON.stringify(result,undefined,2), '[\n  {\n    "st": {\n      "type": "ID",\n      "left": {\n        "type": "+",\n        "left": {\n          "type": "*",\n          "left": 3,\n          "right": 4\n        },\n        "right": 2\n      }\n    }\n  },\n  {\n    "A": {\n      "type": "ID",\n      "left": {\n        "type": "+",\n        "left": {\n          "type": "*",\n          "left": 3,\n          "right": 4\n        },\n        "right": 2\n      }\n    },\n    "t": {\n      "type": "ID",\n      "left": 3\n    },\n    "j": {\n      "type": "ID",\n      "left": "b"\n    },\n    "B": {\n      "type": "ID",\n      "left": {\n        "type": "/",\n        "left": 6,\n        "right": "i"\n      }\n    }\n  }\n]');
+    try {
+      var result = calculator.parse("var a, b; procedure jruvi(); h = a + b; b = h;.");
+      result = (JSON.stringify(result,undefined,2));
+    } catch (e) {
+      result = (String(e));
+    }
+    assert.deepEqual(result, 'Error: Cant use constant or procedure:  h');
   });
   
   test('Variable no definida.', function(){  
-    var result = calculator.parse("var a, c; procedure escala(d); a = d*c; call escala(b)");
-    assert.deepEqual(JSON.stringify(result,undefined,2), '[\n  {\n    "st": {\n      "type": "ID",\n      "left": {\n        "type": "+",\n        "left": {\n          "type": "*",\n          "left": 3,\n          "right": 4\n        },\n        "right": 2\n      }\n    }\n  },\n  {\n    "A": {\n      "type": "ID",\n      "left": {\n        "type": "+",\n        "left": {\n          "type": "*",\n          "left": 3,\n          "right": 4\n        },\n        "right": 2\n      }\n    },\n    "t": {\n      "type": "ID",\n      "left": 3\n    },\n    "j": {\n      "type": "ID",\n      "left": "b"\n    },\n    "B": {\n      "type": "ID",\n      "left": {\n        "type": "/",\n        "left": 6,\n        "right": "i"\n      }\n    }\n  }\n]');
+    try {
+      var result = calculator.parse("var a, c; procedure escala(d); a = d*c; call escala(b)");
+      result = (JSON.stringify(result,undefined,2));
+    } catch (e) {
+      result = (String(e));
+    }
+    assert.deepEqual(result, 'Error: Cant use constant or procedure:  h');
   });
   
   test('Error gramatico', function(){  
