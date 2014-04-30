839e70b801d7cf890fabe3f7743f76b595200d96
Cristian Luis <alu0100693386@ull.edu.es>
2014-04-30 15:22:31 +0100

    Preparando pruebas 4

--- public/test.js
+++ public/test.js
@@ -2,10 +2,9 @@ var assert = chai.assert; //la variable assert contiene los asertos que se puede
 
 suite( 'Analizador Sintacsis + Ambito', function(){  //Suite equivale al describle en RAKE
   
-  test('Ambito procedure.', function(){  
+  test('Admision de procedures paralelos.', function(){  
     var result = calculator.parse("var a, b, c; procedure doble(k); k = k * 2; procedure triple(k);k = k * 3; call doble(b) .");
-    assert.deepEqual(JSON.stringify(result,undefined,2), '[\n  {\n    "st": {\n      "type": "ID",\n      "left": {\n        "type": "-",\n        "left": {\n          "type": "-",\n          "left": 3,\n          "right": 4\n        },\n        "right": 1\n      }\n    }\n  },\n  {\n    "A": {\n      "type": "ID",\n      "left": {\n        "type": "-",\n        "left": {\n          "type": "-",\n          "left": 3,\n          "right": 4\n        },\n        "right": 1\n      }\n    }\n  }\n]'); 
-  });
+    assert.deepEqual(JSON.stringify(result,undefined,2), '[\n  {\n    "V": {\n      "type": "VAR",\n      "variables": [\n        {\n          "type": "VAR",\n          "id": "a"\n        },\n        {\n          "type": "VAR",\n          "id": "b"\n        },\n        "c"\n      ]\n    },\n    "proc": [\n      {\n        "type": "procedure",\n        "nombre": "doble",\n        "right": {\n          "st": {\n            "type": "ID",\n            "nombre": "k",\n            "left": {\n              "type": "*",\n              "left": {\n                "nombre": "k",\n                "declared_in": "doble"\n              },\n              "right": 2\n            },\n            "declared_in": "doble"\n          }\n        },\n        "symboltable": {\n          "name": "doble",\n          "father": "global",\n          "vars": {\n            "k": {\n              "type": "var"\n            }\n          }\n        }\n      },\n      {\n        "type": "procedure",\n        "nombre": "triple",\n        "right": {\n          "st": {\n            "type": "ID",\n            "nombre": "k",\n            "left": {\n              "type": "*",\n              "left": {\n                "nombre": "k",\n                "declared_in": "triple"\n              },\n              "right": 3\n            },\n            "declared_in": "triple"\n          }\n        },\n        "symboltable": {\n          "name": "triple",\n          "father": "global",\n          "vars": {\n            "k": {\n              "type": "var"\n            }\n          }\n        }\n      }\n    ],\n    "st": {\n      "type": "call",\n      "id": "doble",\n      "lista": [\n        {\n          "nombre": "b",\n          "declared_in": "global"\n        }\n      ]\n    }\n  },\n  [\n    {\n      "name": "global",\n      "father": null,\n      "vars": {\n        "c": {\n          "type": "var"\n        },\n        "b": {\n          "type": "var"\n        },\n        "a": {\n          "type": "var"\n        },\n        "doble": {\n          "type": "proc",\n          "longitud": 1\n        },\n        "triple": {\n          "type": "proc",\n          "longitud": 1\n        }\n      }\n    }\n  ]\n]');  });
   
   test('Asignacion a constante.', function(){  
     try {
@@ -24,7 +23,7 @@ suite( 'Analizador Sintacsis + Ambito', function(){  //Suite equivale al describ
     } catch (e) {
       result = (String(e));
     }
-    assert.deepEqual(result, 'Error: Cant use constant or procedure:  b');
+    assert.deepEqual(result, 'Error: bis not defined');
   });
   
   test('Error gramatico', function(){  
