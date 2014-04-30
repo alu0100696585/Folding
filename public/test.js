904e1c49191290186074b32e065db961dc9f0045
Cristian Luis <alu0100693386@ull.edu.es>
2014-04-30 15:18:52 +0100

    Preparando pruebas 3

--- public/test.js
+++ public/test.js
@@ -3,13 +3,13 @@ var assert = chai.assert; //la variable assert contiene los asertos que se puede
 suite( 'Analizador Sintacsis + Ambito', function(){  //Suite equivale al describle en RAKE
   
   test('Ambito procedure.', function(){  
-    var result = calculator.parse("var a, b, c; procedure doble(k); k = k * 2; procedure triple(k); k = k * 3; call double(a); call double(b); .");
+    var result = calculator.parse("var a, b, c; procedure doble(k); k = k * 2; procedure triple(k);k = k * 3; call doble(b) .");
     assert.deepEqual(JSON.stringify(result,undefined,2), '[\n  {\n    "st": {\n      "type": "ID",\n      "left": {\n        "type": "-",\n        "left": {\n          "type": "-",\n          "left": 3,\n          "right": 4\n        },\n        "right": 1\n      }\n    }\n  },\n  {\n    "A": {\n      "type": "ID",\n      "left": {\n        "type": "-",\n        "left": {\n          "type": "-",\n          "left": 3,\n          "right": 4\n        },\n        "right": 1\n      }\n    }\n  }\n]'); 
   });
   
-  test('Variable no definida.', function(){  
+  test('Asignacion a constante.', function(){  
     try {
-      var result = calculator.parse("var a, b; procedure jruvi(); h = a + b; b = h;.");
+      var result = calculator.parse("const h = 9; var a, b; procedure jruvi(); h = a + b; b = h;.");
       result = (JSON.stringify(result,undefined,2));
     } catch (e) {
       result = (String(e));
@@ -19,12 +19,12 @@ suite( 'Analizador Sintacsis + Ambito', function(){  //Suite equivale al describ
   
   test('Variable no definida.', function(){  
     try {
-      var result = calculator.parse("var a, c; procedure escala(d); a = d*c; call escala(b)");
+      var result = calculator.parse("var a, c; procedure escala(d); a = d*c; call escala(b).");
       result = (JSON.stringify(result,undefined,2));
     } catch (e) {
       result = (String(e));
     }
-    assert.deepEqual(result, 'Error: Cant use constant or procedure:  h');
+    assert.deepEqual(result, 'Error: Cant use constant or procedure:  b');
   });
   
   test('Error gramatico', function(){  
